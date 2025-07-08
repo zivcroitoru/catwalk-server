127e6357821a85a84c2485cf1a94a6e9a3ce95e0
may861 <maysis6@gmail.com>
2025-07-08 12:45:30 +0300

    Mey server test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("cat2 Server is live"));
+app.get("/", (req, res) => res.send("May2 Server is live"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`cat2 Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`May2 Server running on port ${PORT}`));
