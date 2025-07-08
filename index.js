e04217bdb127392d2d996712f799321a577f8f9d
zivcroitoru <zuverc@gmail.com>
2025-07-08 12:41:32 +0300

    cat2 server test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("cat Server is live"));
+app.get("/", (req, res) => res.send("cat2 Server is live"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`cat Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`cat2 Server running on port ${PORT}`));
