e28226d2c001605c670f74eb6175dc5eee8cedea
may861 <maysis6@gmail.com>
2025-07-15 17:51:30 +0300

    back to username and password

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
-const passport = require('passport');
-require('./passport')
+
 require("dotenv").config();
 const DB = require('./db');
 
@@ -26,31 +25,24 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false,
+    secure: false, // set to true in production with HTTPS
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24
+    maxAge: 1000 * 60 * 60 * 24 // 1 day
   }
 }));
 
-// Passport middleware
-app.use(passport.initialize());
-app.use(passport.session())
 
+// Plug in all routes
 app.use('/auth', authRoutes);
 app.use('/cats', catsRoutes);
 app.use('/players', playersRoutes);
 app.use('/shop', shopRoutes);
 
 
-app.get('/api/test', async (req, res) => {
-  try {
-    const response = await DB.query("SELECT * FROM users");
-    console.log(response.rows);
-    res.json({ message: 'Hello from server' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Server error");
-  }
+app.get('/api/test', (req, res) => {
+  DB.query("select * from users")
+    .then((response) => { console.log(response.rows) });
+  res.json({ message: 'Hello from server' });
 });
 
 app.get('/api/wow', (req, res) => {
@@ -64,39 +56,9 @@ app.get('/api/wow', (req, res) => {
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).send("ERROR");
+      res.status(404).send("ERROR");
     })
 });
-
-// Route to start Google login
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile'] })
-);
-
-// Google redirects here after login
-app.get('/auth/google/callback',
-  passport.authenticate('google', {
-    failureRedirect: '/login-failed',
-    successRedirect: '/login-success'  // <- You can change this
-  })
-);
-
-// To check who is logged in
-app.get('/auth/me', (req, res) => {
-  if (req.user) {
-    res.json({ user: req.user });
-  } else {
-    res.status(401).json({ error: 'Not logged in' });
-  }
-});
-
-// To log out
-app.get('/auth/logout', (req, res) => {
-  req.logout(() => {
-    res.redirect('/');
-  });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
