c1c53ecda654cba139893aca7fb3687f5f47579c
may861 <maysis6@gmail.com>
2025-07-29 18:16:42 +0300

    cors wildcard chhecking

--- index.js
+++ index.js
@@ -20,15 +20,21 @@ const PORT = 3000;
 
 const devAllowedOrigins = [
   'http://127.0.0.1:5500',
-  'http://localhost:5500',
+  'http://localhost:5500'
 ];
 
+// app.use(cors({
+//   origin: process.env.NODE_ENV === 'production'
+//     ? process.env.FRONTEND_URL
+//     : devAllowedOrigins,
+//   credentials: true,
+// }));
+
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? process.env.FRONTEND_URL
-    : devAllowedOrigins,
-  credentials: true,
+  origin: '*',      // Allow all origins
+  credentials: true, // Note: credentials cannot be used with '*', see below
 }));
+
 
 const io = new Server(server);
 
