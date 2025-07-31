cd316d279cba82795f311018221a60eb857cebe5
zivcroitoru <zuverc@gmail.com>
2025-07-31 23:29:13 +0300

    all origins in mailbox

--- index.js
+++ index.js
@@ -43,13 +43,7 @@ const io = new Server(server, {
 // ───────────── CORS Config ─────────────
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, origin);
-    } else {
-      callback(new Error('Not allowed by CORS: ' + origin));
-    }
-  },
+  origin: '*',
   credentials: true
 }));
 
