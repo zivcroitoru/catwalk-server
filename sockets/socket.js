904593bb686cbbc7ee20bd8e0a6ad64bca423fd7
Peleg Ben-Dor <peleg.bendor@gmail.com>
2025-08-19 23:01:45 +0300

    fashion show responsive

--- sockets/socket.js
+++ sockets/socket.js
@@ -5,9 +5,8 @@ import DB from "../db.js";
 // ═══════════════════════════════════════════════════════════════
 
 // Fashion Show Constants
-const PARTICIPANTS_IN_ROOM = 2;
-// const VOTING_TIMER = 5;
-const VOTING_TIMER = 60000;
+const PARTICIPANTS_IN_ROOM = 5;
+const VOTING_TIMER = 60;
 
 // Global waiting room
 let waitingRoom = {
